9d668a5d2651c2057504018e9f8d7e2c1e7de66d
Natasha <nhodg2@gmail.com>
2023-09-25 20:31:39 -0500

    Update Job Test class with 3 tests testing jobid, constructors setting fields, and job equality

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,53 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.testng.Assert.*;
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId() {
+        //create 2 Job objects using empty constructor
+        Job job1 = new Job();
+        Job job2 = new Job();
+        //assertNotEquals verify Ids are diff
+        assertNotEquals(job1.getId(), job2.getId());
+        //Run test to check
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        //declare and initialize a new Job object
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        //use assertTrue and assertEquals to test the constructor
+        //assigns both the class and value of each field
+        //5 assert statements for each (assertTrue and assertEquals)
+        //assertTrue might be testing the instances
+        //not sure about these
+        assertTrue(job1 instanceof Job);
+        assertTrue(job1.getEmployer() instanceof Employer);
+        assertTrue(job1.getLocation() instanceof Location);
+        assertTrue(job1.getPositionType() instanceof PositionType);
+        assertTrue(job1.getCoreCompetency() instanceof  CoreCompetency);
+
+        //do these fields equal what they should equal
+        assertEquals(job1.getName(), "Product tester");
+        assertEquals(job1.getEmployer().getValue(), "ACME");
+        assertEquals(job1.getLocation().getValue(), "Desert");
+        assertEquals(job1.getPositionType().getValue(), "Quality control");
+        assertEquals(job1.getCoreCompetency().getValue(), "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        //generate 2 Job objects with identical field values EXCEPT for id
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        //test equals returns false
+        boolean isEqual = job1.equals(job2);
+        assertFalse(isEqual);
+        //assertNotEquals(job1, job2); what is the difference between these 2 approaches
+    }
 }
